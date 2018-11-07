a1c863da48e96321b0651638434b842c388e0fb5
Justin Latorre <jlatorre2000@gmail.com>
2018-11-07 09:19:42 -0500

    some framework done

---
+++ src/ATM.java
@@ -0,0 +1,9 @@
+public class ATM {
+	private String account;
+	private Boolean validate;
+	private int dispense;
+	private String menu;
+	private int deposit;
+	
+	/*getter and setters time*/
+}
---
+++ src/Account.java
@@ -0,0 +1,10 @@
+public class Account {
+	private int balance;
+	private int account_number;
+	private String accountOwner;
+	
+	private int withdraw;
+	private int deposit;
+	
+	/*getter and setters time*/
+}
